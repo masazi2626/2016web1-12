5e06ccc749e11e6d577dd1f9f6d8d028042dc784
masazi2626 <luigimark2@i.softbank.jp>
2016-06-27 17:41:27 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -9,26 +9,24 @@ for(var i = 1; i < 101; i++)
   var li = document.createElement('li');
 
   
-  if(i%15==0)
+  if(i%3==0)
   {
-    li.textContent = 'FizzBuzz';
+    li.textContent = 'Fizz';
   }
   else
   {
     li.textContent = i;
   }
   
-  if(i%3==0)
-  {
-    li.textContent = 'Fizz';
-  }
-
   if(i%5==0)
   {
     li.textContent = 'Buzz';    
   }
 
-  
+  if(i%15==0)
+  {
+    li.textContent = 'FizzBuzz';
+  }    
 
   document.getElementById('list').appendChild(li);
 }
