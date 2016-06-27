5b4520097605c33ba5076cd3ac7efb4337aac228
masazi2626 <luigimark2@i.softbank.jp>
2016-06-27 17:22:17 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,12 @@
 document.getElementById('form').onsubmit = function()
 {
   window.alert(document.getElementById('form').word.value);
+  return false;
 };
+
+for(var i = 1; i < 100; i++)
+{
+  var li = document.createElement('li');
+  li.textContent = i;
+  document.getElementById('list').appendChild(li);
+}
